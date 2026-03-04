d48a8e2d3fed5d90d913c49ce770552c17468b53
tanzinaakhter <takhter0305@gmail.com>
2026-03-03 22:58:06 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+/*
+Name: Tanzina Akhter
+Course: CSCI 272
+Lesson Number: Lesson 4
+Code Topic: Copy Constructor Example
+Date: 03/03/2026
+*/
+
+#include <iostream>
+#include "BankAccount.h"
+using namespace std;
+
+int main (){
+    return 0;
+}
+
+/*
+Reflection – BankAccount.cpp
+
+What I understood from this example:
+From this example, I understood how encapsulation works in a practical situation like a bank account. 
+
+Any difficulties you faced:
+I had to carefully think about the withdraw function and make sure that money cannot be withdrawn if the balance is not enough. 
+
+What you searched or researched online (if anything):
+I reviewed my class notes and lecture slides to better understand how the deposit and withdraw functions should work.
+
+Whether you used AI (if yes, explain how):
+No, I did not use AI to complete this assignment. I relied on the lesson slides, class materials, and my own testing to 
+complete and understand the code.
+
+What you learned from completing this code:
+I learned how encapsulation helps protect important data and how to design functions that safely modify private variables. 
+*/
